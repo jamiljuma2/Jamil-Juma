c26b2061d786bd440f1a8cdc7ca2cc764a236a2e
Jamil Juma <jumajamil314@gmail.com>
2026-03-29 04:12:17 +0300

    Update LinkedIn link to new profile URL

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -6,7 +6,7 @@ export const SOCIALS = [
   },
   {
     name: "LinkedIn",
-    url: "https://linkedin.com/in/jamiljuma",
+    url: "https://www.linkedin.com/in/jamil-juma-5b5606343",
     icon: "FaLinkedin",
   },
   {
